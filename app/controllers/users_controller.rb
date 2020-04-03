40e8c074590d9942a93466c1b411b735e51b81c0
1889yasuto <3351vasuto@gmail.com>
2020-04-03 16:21:52 +0900

    インクリメントリサーチ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,39 @@
+
 class UsersController < ApplicationController
+
+  def index
+    @users = User.search(params[:keyword], current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+  
   def edit
   end
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+    redirect_to root_path
     else
-      render :edit
+    render :edit
     end
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email)
   end
 end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
