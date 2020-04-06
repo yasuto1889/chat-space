2a183b9e1cae0e475e0703a0d000cbd40639d4bf
1889yasuto <3351vasuto@gmail.com>
2020-04-06 20:29:37 +0900

    自動デプロイ

--- Gemfile
+++ Gemfile
@@ -67,4 +67,5 @@ gem 'pry-rails'
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'jquery-rails'
+gem 'fog-aws'
 
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   include CarrierWave::MiniMagick
   process resize_to_fit: [800,800]
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
